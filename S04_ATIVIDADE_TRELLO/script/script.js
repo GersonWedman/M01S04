539d97c3aa6c38acaa73106bbc925fbd16daa69c
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2023-01-27 16:24:52 -0300

    Estilo and Script files updated

--- S04_ATIVIDADE_TRELLO/script/script.js
+++ S04_ATIVIDADE_TRELLO/script/script.js
@@ -1,5 +1,5 @@
-//inicia a constante teste vazia.
-const teste = [];
+//inicia a constante que armazena os clientes, vazia.
+const clientes = [];
 
 //cria o formCadastro que será o eventListener de form-Cadastro.
 const formCadastro = document.getElementById('form-Cadastro');
@@ -14,11 +14,13 @@ formCadastro.addEventListener('submit', (event) => {
     senha1: event.target.senha1.value,
     senha2: event.target.senha2.value,
   };
-  teste.push(validacao);
-  console.log(teste);
+
+  //   validação de senha - só adiciona os itens de validação em clientes caso senha esteja ok.
   if (senha1.value !== senha2.value) {
-    alert('Senhas diferentes!');
+    alert('Senhas diferentes! Corrigir');
   } else {
-    alert('Senhas conferem!');
+    alert('Cliente criado com sucesso!');
+    clientes.push(validacao);
+    console.log(clientes);
   }
 });
